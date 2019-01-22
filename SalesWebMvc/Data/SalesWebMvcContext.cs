eed615c784491075c021977eac250a2a950efbb7
Hugo Pedro <hugopedro098@gmail.com>
2019-01-22 20:22:11 -0200

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -12,7 +12,10 @@ namespace SalesWebMvc.Models
             : base(options)
         {
         }
-
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        // vou apagar o SalesWebMvc.Models. pq ja é o nome do namespace
+        //public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models
 {
@@ -9,5 +8,27 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+        //lembrando que nao se deve por argumentos que sao coleções no construtor
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        { // pegando cada vendedor da minha lista chamando o totalsales do vendedor naquele periodo inicial e final e ai faço uma soma desse resultado pra todos os vendedores do meu departamento
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
     }
 }
